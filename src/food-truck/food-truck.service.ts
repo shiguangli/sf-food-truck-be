195067796ec06c6d1b8a585b27c734da480aa700
ShiguangLi <lishiguang@outlook.com>
2024-05-10 17:23:42 +0800

    v1.0.0 Search API - handle empty input

--- src/food-truck/food-truck.service.ts
+++ src/food-truck/food-truck.service.ts
@@ -5,12 +5,16 @@ import mobileFoodTruckData from './mobile_food_truck.json';
 @Injectable()
 export class FoodTruckService {
   getListByUserInput(input): any[] {
-    const result = []
-    mobileFoodTruckData.filter((truck) => {
-      if (truck.FoodItems.toLowerCase().includes(input.toLowerCase())) {
-        result.push(truck)
-      }
-    })
-    return result;
+    let result = []
+    if (input) {
+      mobileFoodTruckData.filter((truck) => {
+        if (truck.FoodItems.toLowerCase().includes(input.toLowerCase())) {
+          result.push(truck)
+        }
+      })
+    } else {
+      result = mobileFoodTruckData
+    }
+    return result
   }
 }
